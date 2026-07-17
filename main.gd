ed0e76f35e0a76c91df47cdb0efd720c6ec76174
SpeedyVelcro <32414336+SpeedyVelcro@users.noreply.github.com>
2026-07-17 19:04:16 +0100

    Implement selected title label

--- main.gd
+++ main.gd
@@ -1,7 +1,10 @@
 extends Node
 
-@onready var _description_label := $Control/MarginContainer/VBoxContainer/HBoxContainer2/SVAboutSelectedDescriptionLabel
-@onready var _description_text_edit := $Control/MarginContainer/VBoxContainer/HBoxContainer2/SVAboutSelectedDescriptionTextEdit
+@onready var _description_label := $Control/MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer/SVAboutSelectedDescriptionLabel
+@onready var _description_text_edit := $Control/MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer/SVAboutSelectedDescriptionTextEdit
+@onready var _title_label := $Control/MarginContainer/VBoxContainer/HBoxContainer2/VBoxContainer/SVAboutSelectedTitleLabel
+@onready var _show_title_button := $Control/MarginContainer/VBoxContainer/HBoxContainer/ShowTitleButton
+@onready var _hide_title_button := $Control/MarginContainer/VBoxContainer/HBoxContainer/HideTitleButton
 
 
 func _on_show_label_button_pressed() -> void:
@@ -12,3 +15,15 @@ func _on_show_label_button_pressed() -> void:
 func _on_show_text_edit_button_pressed() -> void:
 	_description_label.visible = false
 	_description_text_edit.visible = true
+
+
+func _on_show_title_button_pressed() -> void:
+	_show_title_button.visible = false
+	_hide_title_button.visible = true
+	_title_label.visible = true
+
+
+func _on_hide_title_button_pressed() -> void:
+	_show_title_button.visible = true
+	_hide_title_button.visible = false
+	_title_label.visible = false
